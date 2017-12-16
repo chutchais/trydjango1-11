7f7dad8cdb198c2f90f392c22c7fef8991273d11
Chutchai SuebKlang <chutchais@iMacMacLCB.local>
2017-12-16 17:48:46 +0700

    add more files

--- shopify/profiles/views.py
+++ shopify/profiles/views.py
@@ -1,14 +1,33 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 
-from django.shortcuts import render,get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render,get_object_or_404,redirect
+from django.views.generic import DetailView,View
 
 # Create your views here.
 from menus.models import Item
 from resturants.models import RestaurantLocation
 
+from .models import Profile
+
+
 User = get_user_model()
+
+class ProfileFollowToggle(LoginRequiredMixin,View):
+	def post(self,request,*args,**kwargs):
+		user_to_toggle = request.POST.get("username")
+		profile_,is_following=Profile.objects.toggle_follow(request.user,user_to_toggle)
+		# profile_ = Profile.objects.get(user__username__iexact=user_to_toggle)
+		# user = request.user
+		# if user in profile_.followers.all():
+		# 	print('Remove')
+		# 	profile_.followers.remove(user)
+		# else:
+		# 	print ('Add')
+		# 	profile_.followers.add(user)
+
+		return redirect(f"/u/{profile_.user.username}/")
 
 class ProfileDetailView(DetailView):
 	template_name ='profiles/user.html'
@@ -22,6 +41,10 @@ class ProfileDetailView(DetailView):
 	def get_context_data(self,*args,**kwargs):
 		context = super(ProfileDetailView,self).get_context_data(*args,**kwargs)
 		user = context['user']
+		is_following = False
+		if user.profile in self.request.user.is_following.all():
+			is_following = True
+		context['is_following'] = is_following
 		query = self.request.GET.get('q')
 		items_exists = Item.objects.filter(user=user)
 		qs = RestaurantLocation.objects.filter(owner=user)
--- shopify/shopify/urls.py
+++ shopify/shopify/urls.py
@@ -18,15 +18,20 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from django.contrib.auth.views import LoginView
+from profiles.views import ProfileFollowToggle
+from menus.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', LoginView.as_view(),name='login'),
-    url(r'^$', TemplateView.as_view(template_name='home.html'),name='home'),
+    # url(r'^$', TemplateView.as_view(template_name='home.html'),name='home'),
+    url(r'^$', HomeView.as_view(),name='home'),
+    url(r'^profile-follow/$', ProfileFollowToggle.as_view(),name='follow'),
     url(r'^about/', TemplateView.as_view(template_name='about.html'),name='about'),
     # url(r'^restaurants/create/$', RestaurantCreateView.as_view(),name='restaurants-create'),
     # url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view(),name='restaurants-details'),
     # url(r'^restaurants/', RestaurantListView.as_view(),name='restaurants'),
+    url(r'^profile-follow/$', ProfileFollowToggle.as_view(),name='follow'),
     url(r'^u/', include('profiles.urls',namespace='profiles')),
     url(r'^items/', include('menus.urls',namespace='menus')),
     url(r'^restaurants/', include('resturants.urls',namespace='restaurants')),
